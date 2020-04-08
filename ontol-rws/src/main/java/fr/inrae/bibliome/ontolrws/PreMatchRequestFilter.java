bbeec098e70cd613c1e2ab83e23c8e3e84f3b928
FPa-inrae <frederic.papazian@inrae.fr>
2020-04-08 18:39:21 +0200

    user's passwords are now stored as unsalted MD5 digests

--- ontol-rws/src/main/java/fr/inrae/bibliome/ontolrws/PreMatchRequestFilter.java
+++ ontol-rws/src/main/java/fr/inrae/bibliome/ontolrws/PreMatchRequestFilter.java
@@ -3,14 +3,19 @@ package fr.inrae.bibliome.ontolrws;
 import fr.inrae.bibliome.ontolrws.Settings.Settings;
 import fr.inrae.bibliome.ontolrws.Settings.User;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -49,8 +54,18 @@ public class PreMatchRequestFilter implements ContainerRequestFilter {
 
                 if (userPass.length == 2) {
                     String userName = userPass[0];
+                    //unsalted hashing with MD5
+                    String hashdPass = null;
+                    try {
+                        MessageDigest md = MessageDigest.getInstance("MD5");
+                        byte[] digest = md.digest(userPass[1].getBytes("UTF-8"));
+                        hashdPass = DatatypeConverter.printHexBinary(digest).toUpperCase();
+                    } catch (NoSuchAlgorithmException ex) {
+                        Logger.getLogger(PreMatchRequestFilter.class.getName()).log(Level.SEVERE, "Unable to perform MD5 digest", ex);
+                    }
+
                     //retrieved user corresponding to provided name and password
-                    Optional<User> authUser = app.getSettings().getAuthenticatedUser(userName, userPass[1]);
+                    Optional<User> authUser = app.getSettings().getAuthenticatedUser(userName, hashdPass);
 
                     if (authUser.isPresent()) {
                         authenticated = true;
