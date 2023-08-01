de4deab47d08fb69e2ed9793fdd292d4f68243b5
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2023-08-01 16:02:15 +0200

    fixed bugs and added caseInsensitive param

--- alvisae-utils/check-annotations/entity-same-property.py
+++ alvisae-utils/check-annotations/entity-same-property.py
@@ -4,23 +4,30 @@ import collections
 
 CORPUS = alvisnlp.Corpus.parse_json(sys.stdin)
 PROPERTY = CORPUS.params['property']
+ci = CORPUS.params['caseInsensitive']
+CI = ci == 'yes' or ci == 'y' or ci == 'true'
 DICT = collections.defaultdict(lambda: collections.defaultdict(list))
 for doc in CORPUS.documents:
     for sec in doc.sections:
         for rel in sec.relations:
             for t in rel.tuples:
-                form = t.features.get_last('form')
+                form = t.features['form']
+                if CI:
+                    form = form.lower()
                 value = ','.join(sorted(t.features.get(PROPERTY)))
                 DICT[form][value].append(t)
 
 
 def tuple_str(t):
     doc = t.relation.section.document
-    return '[%s/%s %s-%s]' % (doc.identifier, doc.features.get_last('external-id'), t.features.get_last('start_'), t.features.get_last('end_'))
+    return '[c=%s (%s), d=%s (%s), o=%s-%s]' % (doc.features['campaign-id'], doc.features['campaign-name'], doc.identifier, doc.features['external-id'], t.features['start_'], t.features['end_'])
 
 
 with open(CORPUS.params['outFile'], 'w') as f:
+    f.write('FORM\tPROPERTY (%s)\tN_OCC\tOCCURRENCES\n' % PROPERTY)
     for form, values in DICT.items():
         if len(values) >= 2:
             for value, tuples in values.items():
-                f.write('\t'.join([form, values, '; '.join(tuple_str(t) for t in tuples)]))
+                f.write('\t'.join([form, value, str(len(tuples)), '; '.join(tuple_str(t) for t in tuples)]))
+                f.write('\n')
+CORPUS.write_events_json(sys.stdout)
